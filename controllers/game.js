b6fadb0198ca156e305d38da16ec27937bebfbd9
Ryan Flood <floodric@gmail.com>
2014-01-14 23:43:48 +0000

    skeletoned out game controller, mostly testing new server

--- controllers/game.js
+++ controllers/game.js
@@ -1,5 +1,20 @@
+function checkGame(game){
+  if(typeof(game) != "object"){
+    return false;
+  }
+  if(typeof(game.state) == "undefined"){
+    return false; // no game state
+  }
+}
 
 
+function createGame(){
+
+}
+
+function endGame(game){
+  checkGame(game);
+}
 
 exports.startGame = createGame;
 exports.endGame = endGame;
